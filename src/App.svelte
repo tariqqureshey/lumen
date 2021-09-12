7a192f678dee1df91f1ccc77f9e61910c9747af7
tariqqureshey <tariqqureshey@gmail.com>
2021-09-12 19:01:35 -0400

    Setup basic Routes
    Added necessecary dependencies

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import About from "./pages/About.svelte";
+  import Checkout from "./pages/Checkout.svelte";
+  import Home from "./pages/Home.svelte";
+  import Login from "./pages/Login.svelte";
+  import Products from "./pages/Products.svelte";
+  import ProductTemplate from "./pages/ProductTemplate.svelte";
+  import { Router, Route, Switch, Link } from "react-router";
+</script>
+
+<Router>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/products">Products</Link>
+  </nav>
+  <Route path="/" component={Home} />
+  <Route path="/about" component={About} />
+  <Route path="/login" component={Login} />
+  <Route path="/products" component={Products} />
+  <Route path="/products/:id" component={ProductTemplate} />
+</Router>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/pages/Checkout.svelte
@@ -0,0 +1,1 @@
+<h1>Welcome to checkout</h1>
