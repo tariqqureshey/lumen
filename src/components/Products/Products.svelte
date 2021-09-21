7ce3b67800404b6bf3e717b398682ebb3671e261
tariqqureshey <tariqqureshey@gmail.com>
2021-09-20 20:46:52 -0400

    Better subscribe and unsubscribe

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,19 +1,9 @@
 <script>
   import { onMount, onDestroy } from "svelte";
   import products from "../../stores/defaultProducts";
-  let localProducts = [];
-  let unsubscribe;
-  onMount(() => {
-    unsubscribe = products.subscribe((value) => {
-      localProducts = value;
-    });
-  });
-  onDestroy(() => {
-    unsubscribe();
-  });
 </script>
 
 <h1>Products component</h1>
-{#each localProducts as product}
+{#each $products as product}
   <h2>{product.title}</h2>
 {/each}
